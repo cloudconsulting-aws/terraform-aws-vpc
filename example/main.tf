4f4fdf11939cda836ae8adc846f0c1217157a4d3
Asaph Tinoco <asaph@sendwave.com>
2025-06-09 19:09:41 +0200

    IAWS1: AZS fixing + Naming convention

--- example/main.tf
+++ example/main.tf
@@ -1,3 +1,7 @@
+data "aws_availability_zones" "azs" {
+  state = "available"
+}
+
 module "vpc" {
   source                  = "../"
   vpc_cidr_block          = "10.0.0.0/16"
@@ -5,6 +9,7 @@ module "vpc" {
   private_subnet_count    = 2
   ips_per_subnet_exponent = 8
   project_name            = "example-project"
+  availability_zones      = data.aws_availability_zones.azs.names
   tags = {
     Environment = "example"
     Owner       = "example-owner"
